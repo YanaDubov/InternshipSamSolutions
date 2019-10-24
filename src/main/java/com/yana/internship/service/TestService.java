711b3560b491305ece5023a27270f1d4bf4ae943
Yana <dubov159@gmail.com>
2019-10-24 14:19:28 +0300

    file and console logging

--- src/main/java/com/yana/internship/service/TestService.java
+++ src/main/java/com/yana/internship/service/TestService.java
@@ -2,12 +2,16 @@ package com.yana.internship.service;
 
 import com.yana.internship.bean.TestBean;
 import com.yana.internship.repository.TestRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class TestService {
+    private static final Logger logger
+            = LoggerFactory.getLogger(TestService.class);
     TestRepository testRepository;
 
     @Autowired
@@ -16,24 +20,35 @@ public class TestService {
     }
 
     public List<TestBean> getAll(){
+        logger.info("Start loading all beans");
         List<TestBean> all = testRepository.getAll();
+        logger.info("{} number of beans has been loaded",all.size());
         return all;
     }
 
     public TestBean getById(int id){
-        return testRepository.getById(id);
+        TestBean bean = testRepository.getById(id);
+        logger.info("Got bean by id {}", bean.getId());
+        return bean;
     }
 
     public TestBean deleteById(int id)
     {
+        logger.info("Delete bean by id {}", id);
         return testRepository.deleteById(id);
     }
 
     public TestBean create(TestBean bean){
-        return testRepository.create(bean);
+
+        TestBean testBean = testRepository.create(bean);
+        logger.info("Bean with id {} has been created", testBean.getId());
+        return testBean;
     }
 
     public TestBean update(TestBean bean){
-         return testRepository.update(bean);
+
+        TestBean testBean = testRepository.update(bean);
+        logger.info("Bean with id {} has been updated", testBean.getId());
+        return testBean;
     }
 }
