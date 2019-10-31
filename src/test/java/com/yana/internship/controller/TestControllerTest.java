8b53e1e071ae178e5ef80f8ee0c04a2d6071da34
Yana <dubov159@gmail.com>
2019-10-31 15:41:22 +0300

    integration test with exception

---
+++ src/test/java/com/yana/internship/controller/TestControllerTest.java
@@ -0,0 +1,72 @@
+package com.yana.internship.controller;
+
+import com.yana.internship.bean.TestEntity;
+import com.yana.internship.config.WebConfig;
+import com.yana.internship.service.TestService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfig.class})
+@WebAppConfiguration
+
+public class TestControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+    @MockBean
+    TestService testService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testServletContextAndController() {
+
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(webApplicationContext.getBean(TestController.class));
+    }
+
+    @Test
+    public void getIdTest() throws Exception {
+
+        TestEntity testEntity = new TestEntity();
+        testEntity.setId(1);
+        testEntity.setName("Yana");
+        when(testService.getById(1)).thenReturn(testEntity);
+        this.mockMvc
+                .perform(get("/test/{id}", 1))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
+}
