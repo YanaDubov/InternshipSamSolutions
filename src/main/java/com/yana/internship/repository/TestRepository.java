12060a5b14cb1d393d9fe1a37bf3088238e1af81
Yana <dubov159@gmail.com>
2019-12-26 17:48:59 +0300

    clean up

--- src/main/java/com/yana/internship/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yana.internship.repository;
-
-import com.yana.internship.entity.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestRepository extends CrudRepository<TestEntity,Long> {
-}
